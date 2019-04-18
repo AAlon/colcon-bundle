a56b458d025ceb6cd096b532587b2acf8b2615f1
Matthew Murphy <matmur@amazon.com>
2019-04-18 16:10:11 -0700

    Add support for --pip-requirements
    Add integration test
    
    Remove TODO
    
    Still needs proper unit tests for --pip-requirements
    
    Add --pip3-requirements
    
    Add tests

--- integration/test_workspace/src/test-pip-install/src/test-pip-install.py
+++ integration/test_workspace/src/test-pip-install/src/test-pip-install.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import pyquaternion
+# From py27_requirements.txt
+import packbits
 
 x_rotation = pyquaternion.Quaternion(axis=[1, 0, 0], degrees=10)
 y_rotation = pyquaternion.Quaternion(axis=[0, 1, 0], degrees=9)
